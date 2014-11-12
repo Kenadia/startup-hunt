7431cfb443083a0324558016545b477325dca7d5
Ken Schiller <kenschiller@gmail.com>
2014-11-11 18:35:05 -0600

    Add startup search by location

--- main.py
+++ main.py
@@ -3,11 +3,53 @@ import json
 import requests
 import sys
 
+import pprint
+
+
+BASE_URL = 'https://api.angel.co/1/'
+SEARCH = BASE_URL + 'search?query={query}&type={type}'
+STARTUPS_BY_ID = BASE_URL + 'startups/{startup_id}'
+STARTUPS_BY_TAG = BASE_URL + 'tags/{tag_id}/startups'
+
+
+def pp(x):
+    pprint.PrettyPrinter(indent=4).pprint(x)
+
+
+def json_to_startups(startups_json):
+    return startups_json
+
+
+def get_location_id_by_name(location_name):
+    data = {
+        'query': location_name,
+        'type': 'LocationTag',
+    }
+    response = requests.get(SEARCH.format(**data))
+    return response.json()[0]['id']
+
+
+def get_startups_by_tag(tag_id):
+    '''Returns up to 50 startups that match a location or market tag.'''
+    data = {
+        'tag_id': tag_id,
+    }
+    response = requests.get(STARTUPS_BY_TAG.format(**data))
+    startups_json = response.json()['startups']
+    return json_to_startups(startups_json)
+
+
+def get_startups_by_location(location_name):
+    location_id = get_location_id_by_name(location_name)
+    startups = get_startups_by_tag(location_id)
+    return startups
+
 
 def main():
-    candidate = json.loads(sys.stdin)
-    companies = ['Lob']
-    print companies
+    # candidate = json.loads(sys.stdin)
+    # companies = ['Lob']
+    # print companies
+    pp(get_startups_by_location('San Francisco'))
 
 if __name__ == '__main__':
     main()
