de6c53893097c3deebf7dcc67a432f464a8dc1a4
Ken Schiller <kenschiller@gmail.com>
2014-11-11 18:05:26 -0600

    Add executable Python script with basic I/O

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+import json
+import sys
+
+
+def main():
+    candidate = json.loads(sys.stdin)
+    companies = ['Lob']
+    print companies
+
+if __name__ == '__main__':
+    main()
